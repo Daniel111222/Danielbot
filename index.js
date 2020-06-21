82cf150267ba495e594e9c676c70885764ac5f8e
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-21 17:26:37 +0200

    Duitser close commando verwijderen

--- index.js
+++ index.js
@@ -253,34 +253,8 @@ client.on("message", async message => {
     }
 
 });
- 
-    if (message.channel.parentID == categoryId) {
-
-        const categoryId = "723177277189259344";
- 
-        message.channel.delete();
- 
-    } else {
- 
-        message.channel.send("Gelieve dit commando in een ticket kanaal te doen.");
- 
- 
-    var embedCloseTicket = new discord.RichEmbed()
-        .setTitle("Hoi, " + message.channel.name)
-        .setDescription("Je ticket is gemarkeerd als **compleet**. Wil je een nieuwe maken doe dan !ticket")
-        .setFooter("ticket gesloten");
- 
-    var logChannel = message.guild.channels.find("name", "log");
-    if (!logChannel) return message.channel.send("Kanaal bestaat niet");
- 
-    logChannel.send(embedCloseTicket);
- 
-}
- 
-module.exports.help = {
-    name: "close",
-    description: "Sluit een ticket af"
-}
+    
+    
 
 // async function promptMessage(message, author, time, reactions) {
 
