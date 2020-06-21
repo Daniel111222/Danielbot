6f17a93e033de3d4cb57a12b1f8588dbee8438b2
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-21 17:14:56 +0200

    Door de Duitser

--- index.js
+++ index.js
@@ -254,6 +254,39 @@ client.on("message", async message => {
 
 });
 
+const discord = require("discord.js");
+ 
+module.exports.run = async (bot, message, args) => {
+ 
+    const categoryId = "723177277189259344";
+ 
+    if (message.channel.parentID == categoryId) {
+ 
+        message.channel.delete();
+ 
+    } else {
+ 
+        message.channel.send("Gelieve dit commando in een ticket kanaal te doen.");
+ 
+    }
+ 
+    var embedCloseTicket = new discord.RichEmbed()
+        .setTitle("Hoi, " + message.channel.name)
+        .setDescription("Je ticket is gemarkeerd als **compleet**. Wil je een nieuwe maken doe dan !ticket")
+        .setFooter("ticket gesloten");
+ 
+    var logChannel = message.guild.channels.find("name", "log");
+    if (!logChannel) return message.channel.send("Kanaal bestaat niet");
+ 
+    logChannel.send(embedCloseTicket);
+ 
+}
+ 
+module.exports.help = {
+    name: "close",
+    description: "Sluit een ticket af"
+}
+
 // async function promptMessage(message, author, time, reactions) {
 
 //     time *= 1000;
