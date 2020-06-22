31e28db89bb25e5567ca5a034036ea7846532f61
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-22 22:07:37 +0200

    error nu wel weg bij filter

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ client.on("message", async message => {
                                                                                                             
                                                                                                             }
 
-                                                                                                            if(message.content.toLowerCase().includes("kkr")){
+                                                                                                            if(message.content.toLowerCase().includes("kk")){
 
                                                                                                                 message.delete()
                                                                                                                 
