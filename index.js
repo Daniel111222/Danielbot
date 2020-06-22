19f86716f66f0c240d0f17750b24bd96a3db4e03
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-23 00:34:22 +0200

    EN => NL

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on("message", async message => {
     for (let i = 0; i < swearWords["swearwords"].length; i++) {
         if (msg.includes(swearWords["swearwords"][i])) {
             message.delete();
-            message.reply(`Your message has been deleted because it included one or multiple swearwords.`).then(msg => msg.delete({ timeout: 5000 })).catch(err => {
+            message.reply(`Je bericht is verwijdert, aangezien schelwoorden verboden zijn`).then(msg => msg.delete({ timeout: 5000 })).catch(err => {
                 message.channel.send('\`\`\`🔴 An error has occurred.\`\`\`');
             });
 
