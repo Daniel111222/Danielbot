1e320a227a07e3be837b93ff697c15d86268e86d
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-22 12:52:01 +0200

    close commando

--- index.js
+++ index.js
@@ -261,8 +261,35 @@ client.on("message", async message => {
         });
     }
 
-});
+    if (commands === 'close') {
+
+        const categoryID = "723177277189259344";
+        
+            if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Jij kan deze ticket niet sluiten!");
+        
+            if (message.channel.parentID == categoryID) {
+                message.channel.delete();
+        
+                var closeTicketEmbed = new discord.MessageEmbed()
+                    .setTitle("Ticket | " + message.channel.name)
+                    .setDescription("Het ticket is gemarkeerd als **compleet**!")
+                    .setTimestamp();
+        
+                var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "logs");
+                if (!ticketChannel) return message.reply(`Het kanaal ${ticketChannel} is niet gevonden!`);
+        
+                ticketChannel.send(closeTicketEmbed);
+        
+            } else {
+        
+                message.reply("Je kunt deze command niet buiten een ticket gebruiken!");
+        
+            }
+    }
     
+});
+
+   
     
 
 // async function promptMessage(message, author, time, reactions) {
