ff96803a4e3e4974d6c7cf286bf946963c9a22a9
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-26 19:34:09 +0200

    index.js

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ fs.readdir("./commands/", (err, files) => {
 
 });
 
-bot.on("ready"), async () => {
+bot.on("ready", async () => {
 
     console.log(`${client.user.username} is online.`);
     client.user.setActivity("Testing", { type: "PLAYING" });
 
-};
+});
 
 bot.on("message", async message => {
 
@@ -52,8 +52,8 @@ bot.on("message", async message => {
     var commands = bot.commands.get(command.slice(prefix.length));
 
     if (commands) commands.run(bot, message, arguments);
-    
-    
+
+
     var swearWords = JSON.parse(fs.readFileSync("./swearWords.json"));
 
     var msg = message.content.toLocaleLowerCase().split(" ");
@@ -80,54 +80,55 @@ bot.on("message", async message => {
 
         }
     }
+});
 
-    client.on("guildMemberAdd", member => {
+client.on("guildMemberAdd", member => {
 
-        var role = member.guild.roles.cache.get('721118805107146762');
-        if (!role) return;
-        member.roles.add(role);
-    
-        var channel = member.guild.channels.cache.get('718763245246349315');
-        if (!channel) return;
-    
-        channel.send({
-            embed: {
-                title: `WELKOM`,
-                description: `Welkom **${member.user.username}**!\nWe hebben nu **${member.guild.memberCount}** leden!`,
-                color: "GREEN",
-                timestamp: new Date(),
-                thumbnail: {
-                    url: member.user.avatarURL({ dynamic: true }) ? member.user.avatarURL({ dynamic: true }) : null
-                },
-                footer: {
-                    text: "WELKOM"
-                }
+    var role = member.guild.roles.cache.get('721118805107146762');
+    if (!role) return;
+    member.roles.add(role);
+
+    var channel = member.guild.channels.cache.get('718763245246349315');
+    if (!channel) return;
+
+    channel.send({
+        embed: {
+            title: `WELKOM`,
+            description: `Welkom **${member.user.username}**!\nWe hebben nu **${member.guild.memberCount}** leden!`,
+            color: "GREEN",
+            timestamp: new Date(),
+            thumbnail: {
+                url: member.user.avatarURL({ dynamic: true }) ? member.user.avatarURL({ dynamic: true }) : null
+            },
+            footer: {
+                text: "WELKOM"
             }
-        });
-    
+        }
     });
 
-    client.on("guildMemberRemove", member => {
+});
 
-        var channel = member.guild.channels.cache.get('718763245246349315');
-        if (!channel) return;
-    
-        channel.send({
-            embed: {
-                title: `TOT ZIENS`,
-                description: `Jammer dat je weggaat **${member.user.username}**!\nWe hebben nu nog **${member.guild.memberCount}** leden!`,
-                color: "RED",
-                timestamp: new Date(),
-                thumbnail: {
-                    url: member.user.avatarURL({ dynamic: true }) ? member.user.avatarURL({ dynamic: true }) : null
-                },
-                footer: {
-                    text: "TOT ZIENS"
-                }
+client.on("guildMemberRemove", member => {
+
+    var channel = member.guild.channels.cache.get('718763245246349315');
+    if (!channel) return;
+
+    channel.send({
+        embed: {
+            title: `TOT ZIENS`,
+            description: `Jammer dat je weggaat **${member.user.username}**!\nWe hebben nu nog **${member.guild.memberCount}** leden!`,
+            color: "RED",
+            timestamp: new Date(),
+            thumbnail: {
+                url: member.user.avatarURL({ dynamic: true }) ? member.user.avatarURL({ dynamic: true }) : null
+            },
+            footer: {
+                text: "TOT ZIENS"
             }
-        });
-    
+        }
     });
+
+});
 
     // async function promptMessage(message, author, time, reactions) {
 
@@ -142,5 +143,3 @@ bot.on("message", async message => {
     //     return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
 
     // }
-
-});
