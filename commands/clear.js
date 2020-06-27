186254546cad587ce9d367944aab1fd66c101ce0
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-27 14:56:53 +0200

    clear cmd spartacus

--- commands/clear.js
+++ commands/clear.js
@@ -1,35 +1,44 @@
 const discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-
-    // !clear aantal
-
-    if (!message.member.hasPermission("MANAGE_MESSAGE")) return message.reply("Je hebt geen toestemming om dit te doen.");
-
-    if (!args[0]) return message.reply("Geef een getal op.");
-
-    if (Number.isInteger(parseInt(args[0]))) {
-
+module.exports.run = async(bot, message, args) =>{
+  
+    // if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Jij kunt deze actie niet uitvoeren");
+  
+    var noArgsEmbed = new discord.RichEmbed()
+        .setTitle("Geen geldig aantal gevonden")
+        .setDescription("Geef een aantal tussen de 1-99")
+        .addField("Usage:", "dh/clear {aantal}")
+        .setTimestamp();
+    
+    var noAmountEmbed = new discord.RichEmbed()
+        .setTitle("Geen getal gevonden")
+        .setDescription("Geef een getal tussen de 1-99")
+        .setTimestamp();
+  
+    var deleteEmbed = new discord.RichEmbed()
+        .setTitle("Gelukt!")
+        .setDescription(`Ik heb met succes ${parseInt(args[0])} berichten verwijderd!`)
+        .setTimestamp();
+  
+    if(!args[0]|| args[0] < 1 || args[0] >= 100) return message.channel.send(noArgsEmbed);
+  
+    if(Number.isInteger(parseInt(args[0]))){
+      
         var amount = parseInt(args[0]) + 1;
+      
+        message.channel.bulkDelete(amount).then(() => { 
+        
+          message.channel.send(deleteEmbed).then(msg => msg.delete(3000));
+        
+        });
 
-        message.channel.bulkDelete(amount).then(() => {
-
-            if (args[0] <= 0) {
-                message.reply("Je kan geen 0 berichten verwijderen.").then(msg => msg.delete({ timeout: 3000 }));
-            } else if (args[0] == 1) {
-                message.reply("Er is 1 bericht verwijdert.").then(msg => msg.delete({ timeout: 3000 }));
-            } else {
-                message.reply(`Er zijn ${args[0]} berichten verwijderd`).then(msg => msg.delete({ timeout: 3000 }));
-            }
-
-        })
-
+      
     } else {
-        return message.reply("Geef een getal op.");
+        return message.channel.send(noArgsEmbed);
     }
-
+  
 }
 
 module.exports.help = {
-    name: "clear"
+   name: "clear",
 }
