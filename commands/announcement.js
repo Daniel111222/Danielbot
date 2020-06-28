8a2fa7d1131b3fd84bafb20cacd9a882a28dc0ca
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-28 15:36:37 +0200

    Create announcement.js

---
+++ commands/announcement.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");  
+
+module.exports.run = async(bot, message, args) => {
+
+    // !announcement title | bericht | kleur | kanaal
+
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Sorry jij kan dit niet");
+
+    var seperator = "|";
+
+    if (args[0] == null) {
+
+        var embed = new discord.MessageEmbed()
+            .setTitle("Gebruik")
+            .setColor("#00ee00")
+            .setDescription(`Maak een annoucement aan door gebruik te maken van: \n !announcement title ${seperator} bericht ${seperator} kleur ${seperator} kanaal ${seperator}`);
+
+        return message.reply(embed);
+        
+    }
+
+}
+
+module.exports.help = {
+    name: "announcement"
+}
