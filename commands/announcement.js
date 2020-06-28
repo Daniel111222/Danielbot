9ef4860c4420a880c4c0a409b60d9d89163bf5c0
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-28 21:56:34 +0200

    Update announcement.js

--- commands/announcement.js
+++ commands/announcement.js
@@ -19,6 +19,31 @@ module.exports.run = async(bot, message, args) => {
         
     }
 
+    var argsList = args.join(" ").split(seperator);
+
+    if(argsList[2] === undefined) argsList[2] = "#eeeeee";
+    if(argsList[3] === undefined) argsList[3] = "#chat";
+
+    var options = {
+
+        title: argsList[0],
+        bericht: argsList[1] || "Geen inhoud meegegeven",
+        kleur: argsList[2].trim(),
+        kanaal: argsList[3].trim()
+
+    }
+
+    var announceEmbed = new discord.MessageEmbed()
+        .setTitle("Announcement")
+        .setColor(options.kleur)
+        .setDescription(`Bericht van ${message.author} \n\n ${options.title} \n ${options.bericht}`)
+        .setTimestamp();
+
+    var channel = message.member.guild.channels.cache.find(channels => channels.name === options.kanaal);
+    if (!channel) return message.reply("Dit kanaal is bestaat niet");
+
+    channel.send(announceEmbed);    
+
 }
 
 module.exports.help = {
