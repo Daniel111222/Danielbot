e9fd98e58372e888d8d8817c7d3cbaeb305b598d
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-06-30 00:10:11 +0200

    Update hallo.js

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,9 +2,9 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    return message.channel.send("superfanboygamerNL30 is lief en was een stom grapje");
+    return message.channel.send("Hallo!");
 }
 
 module.exports.help = {
-    name: "superfanboygamerNL30"
+    name: "hallo"
 }
