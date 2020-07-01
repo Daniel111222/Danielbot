ac664f3827336160c4482b764493e4bb51efd301
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-01 21:33:06 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -6,10 +6,8 @@ module.exports.run = async (client, message, args) => {
     .setTitle("info")
     .setColor("#0099ff")
     .addFields(
-        { name: "Dit is De Officiële support server van Daniël bot", value: "[Klik Hier!](https://discord.gg/2E5fhn3)" },
-        // { name: "Dit is De Officiële Youtube Platform", 
-        // value: "[Klik Hier!](https://www.youtube.com/channel/UCZkEU_dw4Bj82yVAkYj_fZg)%22) },
-
+        { name: "Dit is De officiële support server van de bot: Daniël bot", value: "[Klik Hier!](https://discord.gg/2E5fhn3)" },
+        { name: "Dit is de link van de officiële Daniël bot", value: "_De link van de bot zal hier staan, wanneer de bot uit is._"},
     )
     //.setThumbnail("https://zeroserenity.com/wp-content/uploads/2019/07/Discord-Logo-Red.png")
     //.setImage("https://zeroserenity.com/wp-content/uploads/2019/07/Discord-Logo-Red.png")
@@ -18,20 +16,6 @@ module.exports.run = async (client, message, args) => {
 
 return message.channel.send(botEmbed);
 }
-
-    //     var botEmbed = new discord.MessageEmbed()
-    //         .setTitle("Uden Roleplay Informatie")
-    //         .setColor("#0099ff")
-    //         .addField("Dit is De Officiële Burgers Van Deze Discord", Er zijn momenteel ${message.guild.memberCount} leden in de Discord!)
-    //         .addField("Dit is De Officiële Youtube Platform", "[Klik Hier!](https://www.youtube.com/channel/UCZkEU_dw4Bj82yVAkYj_fZg)%22)
-    //         .addField("Dit is De Officiële InstaGram Platform", "[Klik Hier!](https://www.instagram.com/diegovisser7002/)%22)
-    //         .addField("Dit is De Officiële Discord Platform", "[Klik Hier!](https://discord.gg/WmbyWCN)%22)
-    //         .addField("Dit is De Officiële Twitch Platform", "[Klik Hier!](https://www.twitch.tv/diegovisser3)%22)
-    //         .addField("Dit is De Officiële Bot invite Linkje", "[Klik Hier!](https://discord.com/oauth2/authorize?client_id=725301850206633984&scope=bot&permissions=2146958847)%22)
-    //         .addField("Dit is De Officiële Roblox Group Platform", "[Klik Hier!](https://www.roblox.com/groups/6815576/Uden-Roleplay#!/affiliates)%22)
-
-    //         return message.channel.send(botEmbed);
-    // }
 
 module.exports.help = {
     name: "info"
