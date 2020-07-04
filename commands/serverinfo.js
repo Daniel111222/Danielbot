39ab38acaa52ca3cc75b9e54d209e58eaacd716c
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-04 02:44:23 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -12,7 +12,7 @@ module.exports.run = async (client, message, args) => {
             { name: "Totaal members", value: message.guild.memberCount },
             { name: "Aantal servers", value: client.guilds.cache.size }
         )
-        //.setThumbnail("https://zeroserenity.com/wp-content/uploads/2019/07/Discord-Logo-Red.png")
+        .setThumbnail("https://cdn.discordapp.com/attachments/719562600282849324/728585483118051368/BOT_NU_g_OE_d.jpg")
         //.setImage("https://zeroserenity.com/wp-content/uploads/2019/07/Discord-Logo-Red.png")
         .setFooter("Gemaakt door @Daniël#2939 en veel dank aan de helpers.") //, "https://zeroserenity.com/wp-content/uploads/2019/07/Discord-Logo-Red.png")
         .setTimestamp();
