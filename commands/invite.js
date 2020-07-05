7e4ad8333bfa46b86821dce2f788815d5a343a4e
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-05 18:56:45 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,9 +6,9 @@ module.exports.run = async (client, message, args) => {
     .setTitle("invites")
     .setColor("#32a852")
     .addFields(
-        { name: "Dit is de link van de officiële Daniël bot", value: "_De link van de bot zal hier staan, wanneer de bot uit is._"},
-        { name: "Dit is De officiële support server van de bot: Daniël bot", value: "[Klik Hier!](https://discord.gg/2E5fhn3)" },
-        { name: "Dit is De officiële website van de Daniël bots", value: "[Klik Hier!](https://danielbot.webnode.nl/)" },
+        { name: "Dit is de link van de officiële Daniël bot.", value: "_Bot is nog niet klaar._"},
+        { name: "Dit is De officiële support server van de bot: Daniël bot.", value: "[Klik Hier!](https://discord.gg/2E5fhn3)" },
+        { name: "Dit is De officiële website van de Daniël bots.", value: "[Klik Hier!](https://danielbot.webnode.nl/)" },
     )
     .setThumbnail("https://cdn.discordapp.com/attachments/719562600282849324/728585483118051368/BOT_NU_g_OE_d.jpg")
     .setImage("https://cdn.discordapp.com/attachments/719562600282849324/728585483118051368/BOT_NU_g_OE_d.jpg")
