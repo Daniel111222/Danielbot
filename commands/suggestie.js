faa69138d41dfd7607ed0c46c315a18f3d7ec810
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-09 16:18:06 +0200

    Update suggestie.js

--- commands/suggestie.js
+++ commands/suggestie.js
@@ -1,37 +1,37 @@
 const discord = require("discord.js");
- 
+
 module.exports.run = async (bot, message, args) => {
-    
-    // Vang het suggestie op.
-    var suggestie = args.join(" ");
- 
-    // Kijk na als er een suggestie is meegegeven.
-    if (!suggestie) return message.channel.send("Geen suggestie meegegeven gelieve een suggestie mee te geven.");
- 
-    // Maak het embed aan.
-    var suggestieEmbed = new discord.RichEmbed()
-        .setTitle("Nieuwe suggestie")
-        .setColor("#00ff00")
-        .addFields(
-          { name: "suggestie:", value: suggestie},
-          { name: "Ingezonden door:", value: `${message.author}`}
-      )
- 
-    // Vind het kanaal.
-    var suggestieChannel = message.guild.channels.find("id", "718765875104055377");
-    if (!suggestieChannel) return message.guild.send("Kan het kanaal niet vinden");
- 
-    // Verzend het bericht en voeg er reacties aan toe.
-    suggestieChannel.send(suggestieEmbed).then(embedMessage => {
-        embedMessage.react('👍');
-        embedMessage.react('👎');
-    });
- 
-    // Einde.
- 
+
+  // Vang het suggestie op.
+  var suggestie = args.join(" ");
+
+  // Kijk na als er een suggestie is meegegeven.
+  if (!suggestie) return message.channel.send("Geen suggestie meegegeven gelieve een suggestie mee te geven.");
+
+  // Maak het embed aan.
+  var suggestieEmbed = new discord.RichEmbed()
+    .setTitle("Nieuwe suggestie")
+    .setColor("#00ff00")
+    .addFields(
+      { name: "suggestie:", value: suggestie },
+      { name: "Ingezonden door:", value: `${message.author}` }
+    )
+
+  // Vind het kanaal.
+  var suggestieChannel = message.guild.channels.find("id", "718765875104055377");
+  if (!suggestieChannel) return message.guild.send("Kan het kanaal niet vinden");
+
+  // Verzend het bericht en voeg er reacties aan toe.
+  suggestieChannel.send(suggestieEmbed).then(embedMessage => {
+    embedMessage.react('👍');
+    embedMessage.react('👎');
+  });
+
+  // Einde.
+
 }
- 
+
 module.exports.help = {
-    name: "suggestie",
-    description: "Heb je een suggestie. Zet het dan hier en misschien passen we het toe."
+  name: "suggestie",
+  description: "Heb je een suggestie. Zet het dan hier en misschien passen we het toe."
 }
