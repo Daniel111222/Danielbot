7fe725fb4c893f7d463965ea32edeaf1e3e3a87a
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-09 15:50:54 +0200

    init v11 suggestie

--- commands/suggestie.js
+++ commands/suggestie.js
@@ -1,34 +1,35 @@
-const discord = require("discord.js");  
-
-module.exports.run = async(bot, message, args) => {
-
-    //!suggestie {args[0]}
-  
-    var suggestion = args.slice(0).join(" ");
-  
-    // var suggestEmbed = new discord.MessageEmbed()
-    //   .setTitle("Nieuw Idee")
-    //   .setColor("#249643")
-    //   .addField("Door", `${message.author.username}`)
-    //   .addField("Suggestie", suggestion)
-    //   .setTimestamp();
-
-      var suggestEmbed = new discord.MessageEmbed()
-      .setColor("#ff0000")
-      .setFooter("Gemaakt door @Daniël#2939 en veel dank aan de helpers.")
-      .setTimestamp()
-      .setDescription(suggestion)
-
-    var channel = message.member.guild.channels.cache.get("718765875104055377")
-
-    message.channel.send(embedPrompt).then(embedMessage => {
+const discord = require("discord.js");
+ 
+module.exports.run = async (bot, message, args) => {
+    
+    // Vang het idee op.
+    var suggestie = args.join(" ");
+ 
+    // Kijk na als er een idee is meegegeven.
+    if (!suggestie) return message.channel.send("Geen Idee meegegeven gelieve een idee mee te geven.");
+ 
+    // Maak het embed aan.
+    var ideeEmbed = new discord.RichEmbed()
+        .setTitle("Nieuw Idee")
+        .setColor("#00FF00")
+        .addField("suggestie: ", suggestie)
+        .addField("Ingezonden door: ", message.author);
+ 
+    // Vind het kanaal.
+    var ideeChannel = message.guild.channels.find("id", "729242817045594181");
+    if (!ideeChannel) return message.guild.send("Kan het kanaal niet vinden");
+ 
+    // Verzend het bericht en voeg er reacties aan toe.
+    ideeChannel.send(ideeEmbed).then(embedMessage => {
         embedMessage.react('👍');
         embedMessage.react('👎');
     });
  
-  
+    // Einde.
+ 
 }
-  
-  module.exports.help = {
-      name: "suggestie"
-  }
+ 
+module.exports.help = {
+    name: "suggestie",
+    description: "Heb je een idee. Zet het dan hier en misschien passen we het toe."
+}
