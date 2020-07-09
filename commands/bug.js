ff53e5e030257b51c7ad9577d706f7e5761e617e
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-09 17:58:09 +0200

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -2,32 +2,38 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  // Vang het bug op.
-  var bug = args.join(" ");
+    // Vang het bug op.
+    var bug = args.join(" ");
 
-  // Kijk na als er een bug is meegegeven.
-  if (!bug) return message.channel.send("Geen bug opgegeven, gelieve de bug achter !bug te schrijven.");
+    // Kijk na als er een bug is meegegeven.
+    if (!bug) return message.channel.send("Geen bug opgegeven, gelieve de bug achter !bug te schrijven.");
 
-  // Maak het embed aan.
-  var bugEmbed = new discord.MessageEmbed()
-    .setTitle("Nieuwe bug:")
-    .setColor("#00ff00")
-    .addFields(
-      { name: "bug:", value: bug },
-      { name: "Ingezonden door:", value: `${message.author}` }
-    )
+    // Maak het embed aan.
+    var bugEmbed = new discord.MessageEmbed()
+        .setTitle("Nieuwe bug:")
+        .setColor("#00ff00")
+        .addFields(
+            { name: "bug:", value: bug },
+            { name: "Ingezonden door:", value: `${message.author}` }
+        )
 
-  // Vind het kanaal.
-  var bugChannel = message.member.guild.channels.cache.get("718765810625282058")
-  if (!bugChannel) return message.guild.send("Kan het kanaal niet vinden");
+    // Vind het kanaal.
+    var bugChannel = message.member.guild.channels.cache.get("718765810625282058")
+    if (!bugChannel) return message.guild.send("Kan het kanaal niet vinden");
 
-  bugChannel.send(bugEmbed)
+    bugChannel.send(bugEmbed)
 
-  // Einde.
+    var botEmbed = new discord.MessageEmbed()
+        .setTitle(`${message.author}, je but is succesvol naar het bug kanaal gestuurd`)
+        .setColor("#1ac91a")
+
+    return message.channel.send(botEmbed);
+
+    // Einde.
 
 }
 
 module.exports.help = {
-  name: "bug",
-  description: "Heb je een bug gevonden?. Zet het dan hier en we lossen het zo snel mogelijk op."
+    name: "bug",
+    description: "Heb je een bug gevonden?. Zet het dan hier en we lossen het zo snel mogelijk op."
 }
