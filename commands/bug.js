7fa06a5f0d467fa2445891ea510379ba95b06a43
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-09 16:45:47 +0200

    Create bug.js

---
+++ commands/bug.js
@@ -0,0 +1,31 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  // Vang het bug op.
+  var bug = args.join(" ");
+
+  // Kijk na als er een bug is meegegeven.
+  if (!bug) return message.channel.send("Geen bug opgegeven, gelieve de bug achter !bug te schrijven.");
+
+  // Maak het embed aan.
+  var bugEmbed = new discord.MessageEmbed()
+    .setTitle("Nieuwe bug:")
+    .setColor("#00ff00")
+    .addFields(
+      { name: "bug:", value: bug },
+      { name: "Ingezonden door:", value: `${message.author}` }
+    )
+
+  // Vind het kanaal.
+  var bugChannel = message.member.guild.channels.cache.get("718765810625282058")
+  if (!bugChannel) return message.guild.send("Kan het kanaal niet vinden");
+
+  // Einde.
+
+}
+
+module.exports.help = {
+  name: "bug",
+  description: "Heb je een bug gevonden?. Zet het dan hier en we lossen het zo snel mogelijk op."
+}
