05da8c253c6d08bf16e34564f6e45dcf690598ea
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-09 13:55:11 +0200

    Update suggestie.js

--- commands/suggestie.js
+++ commands/suggestie.js
@@ -6,12 +6,20 @@ module.exports.run = async(bot, message, args) => {
   
     var suggestion = args.slice(0).join(" ");
   
-    var suggestEmbed = new discord.MessageEmbed()
-      .setTitle("Nieuw Idee")
-      .setColor("#249643")
-      .addField("Door", `${message.author.username}`)
-      .addField("Suggestie", suggestion)
-      .setTimestamp();
+    // var suggestEmbed = new discord.MessageEmbed()
+    //   .setTitle("Nieuw Idee")
+    //   .setColor("#249643")
+    //   .addField("Door", `${message.author.username}`)
+    //   .addField("Suggestie", suggestion)
+    //   .setTimestamp();
+
+      var suggestEmbed = new discord.MessageEmbed()
+      .setColor("#ff0000")
+      .setFooter("Gemaakt door @Daniël#2939 en veel dank aan de helpers.")
+      .setTimestamp()
+      .setDescription(suggestion)
+
+    var channel = message.member.guild.channels.cache.get("718765875104055377")
 
     message.channel.send(embedPrompt).then(embedMessage => {
         embedMessage.react('👍');
